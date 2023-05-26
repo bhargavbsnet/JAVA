2877f89f7f279c5566bbd15b3c3d9debb2282bc0
bhargavbsnet <bhargavbdrbasnet@gmail.com>
2023-05-26 08:31:45 +0545

    input commited

--- JAVATUTORIALS/second.java
+++ JAVATUTORIALS/second.java
@@ -1,5 +1,13 @@
 package JAVATUTORIALS;
+import java.util.*;
 
 public class second {
-    
+        public static void main(String[] args)
+            {
+                    Scanner sc = new Scanner(System.in);
+                       // String name = sc.next(); this take only single string
+                       String name = sc.nextLine(); //this take whole string with space included
+                        System.out.println(name);
+                    
+            }
 }
