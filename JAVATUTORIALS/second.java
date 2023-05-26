6464ce30530def80982a884c10f0690bc91a892b
bhargavbsnet <bhargavbdrbasnet@gmail.com>
2023-05-26 08:34:11 +0545

    again commited

--- JAVATUTORIALS/second.java
+++ JAVATUTORIALS/second.java
@@ -7,6 +7,8 @@ public class second {
                     Scanner sc = new Scanner(System.in);
                        // String name = sc.next(); this take only single string
                        String name = sc.nextLine(); //this take whole string with space included
+                       //nextInt()
+                       //nextFloat()
                         System.out.println(name);
                     
             }
